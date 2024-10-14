74330845af3d9c3c63d6e429ee489a984be7a901
DmBelkin <143951259+DmBelkin@users.noreply.github.com>
2024-10-14 22:23:08 +0300

    with credentials

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -4,16 +4,12 @@ import com.byteowls.jopencage.model.JOpenCageLatLng;
 import com.byteowls.jopencage.model.JOpenCageResponse;
 import lombok.Setter;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.api.methods.AnswerCallbackQuery;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.io.IOException;
-import java.util.List;
 
 @Setter
 public class Bot extends TelegramLongPollingBot {
@@ -24,6 +20,7 @@ public class Bot extends TelegramLongPollingBot {
 
     private DBConnection connection;
 
+
     private final String text = "/ + название города на латинице, страна(штат, регион) - " +
             "получить дневной прогноз погоды" + "\n" +
             "/ + history - получить историю своих запросов";
@@ -31,14 +28,7 @@ public class Bot extends TelegramLongPollingBot {
 
     public Bot(DBConnection connection) {
         this.connection = connection;
-        InlineKeyboardButton start = InlineKeyboardButton.builder()
-                .text("START").callbackData("start")
-                .build();
-        InlineKeyboardMarkup markup = InlineKeyboardMarkup.builder()
-                .keyboardRow(List.of(start)).build();
-
-        sendMenu(id, "MENU", markup);
-
+        sendText(id, text);
     }
 
     @Override
@@ -53,13 +43,6 @@ public class Bot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        if (update.getCallbackQuery().getData().equals("start")) {
-            try {
-                buttonTap(update.getCallbackQuery().getId());
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
-            }
-        }
         var message = update.getMessage();
         var user = message.getFrom();
         if (message.getText().equals("/history")) {
@@ -107,27 +90,6 @@ public class Bot extends TelegramLongPollingBot {
         }
         return mess;
     }
-
-    public void sendMenu(Long who, String txt, InlineKeyboardMarkup kb){
-        SendMessage sm = SendMessage.builder().chatId(who.toString())
-                .parseMode("HTML").text(txt)
-                .replyMarkup(kb).build();
-
-        try {
-            execute(sm);
-        } catch (TelegramApiException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private void buttonTap(String queryId) throws TelegramApiException {
-        sendText(id, text);
-        AnswerCallbackQuery close = AnswerCallbackQuery.builder()
-                .callbackQueryId(queryId).build();
-
-        execute(close);
-    }
-
 
     public double[] getCoordsByTownName(String name) throws IOException {
         JOpenCageGeocoder jOpenCageGeocoder = new JOpenCageGeocoder(geolocationApiKey);
